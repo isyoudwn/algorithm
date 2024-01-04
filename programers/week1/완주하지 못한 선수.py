6bd8a989536041c79527f9f80f0541e0e1d3f66c
임채연 <dlacodus0407@gmail.com>
2024-01-04 12:49:35 +0900

    [level 1] 완주하지 못한 선수

---
+++ programers/week1/완주하지 못한 선수.py
@@ -0,0 +1,23 @@
+
+def solution(participant, completion):
+
+    participant.sort()
+    completion.sort()
+    
+    for i in range(0, len(participant)) :
+        
+        if i == len(participant) - 1 :
+            return participant[len(participant)-1]
+        
+        if participant[i] != completion[i] :
+            return participant[i] 
+        
+        else :
+            continue
+    
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
+
+result = solution(participant, completion)
+
+print(result)
