9c526464ba0b367e5467ec74a8149e9a86c34c6b
임채연 <dlacodus0407@gmail.com>
2024-01-11 00:15:51 +0900

    [level 2] 소수 찾기

---
+++ programers/week2/소수찾기.py
@@ -0,0 +1,56 @@
+from itertools import permutations
+
+def solution(numbers):
+    answer = 0
+    tempList = []
+    totalList = []
+    result = []
+    
+    # 순열로 뽑아서 모든 경우의 수 list에 넣기
+    for i in range(1, len(numbers) + 1) :
+        tempList.append(list(permutations(list(numbers), i)))
+    
+    # 튜플 list로 만들기
+    for i in range(0, len(tempList)) : 
+        for j in range(0, len(tempList[i])) :
+                totalList.append(list(tempList[i][j]))        
+
+    # 앞자리 0인 거 0빼고 나머지로 만들기
+    for i in range(0, len(totalList)) :
+        if len(totalList[i]) < 2:
+            continue
+        for j in range (0, len(totalList[i])):
+            if totalList[i][j] != '0':
+                totalList[i] = totalList[i][j:]
+                break
+    
+    # 중복 제거
+    for i in range(0, len(totalList)) :
+        result.append(''.join(totalList[i]))
+
+    result = list(set(result))
+    
+    # 소수인지 계산
+    for i in range(0, len(result)) :
+        
+        if result[i] == '0' :
+            continue
+            
+        if result[i] == '1' :
+            continue
+        
+        answer = answer + 1
+    
+        for j in range(2, int(result[i])) :
+            if int(result[i]) % j == 0:
+                answer = answer - 1
+                break
+
+    return answer
+
+
+
+
+
+result = solution("11000")
+print(result)
