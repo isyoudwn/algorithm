06fd8c31f398d60fd9438c2fbd64884f483f096b
임채연 <dlacodus0407@gmail.com>
2024-01-11 10:26:24 +0900

    [practice] dfs 연습

---
+++ programers/week2/dfs.py
@@ -0,0 +1,27 @@
+
+def dfs(graph, start, visited = []) :
+    visited.append(start)
+
+    for node in graph[start] :
+        if node not in visited:
+            dfs(graph, node, visited)
+            print()
+    return visited
+
+
+graph = dict()
+ 
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(graph)
+
+result = dfs(graph, 'A')
