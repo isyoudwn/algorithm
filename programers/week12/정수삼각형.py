f39914be5009010689c37e8ac1514e7f6f05d45e
임채연 <dlacodus0407@gmail.com>
2024-08-22 21:20:15 +0900

    [level 3] 정수 삼각형
    기존코드 주석처리

--- programers/week12/정수삼각형.py
+++ programers/week12/정수삼각형.py
@@ -1,3 +1,34 @@
+# def solution(triangle):
+#     answer = 0
+#     N = len(triangle[len(triangle) - 1])
+#     dp = []
+    
+#     for i in range(0, N):
+#         dp.append([])
+#         for j in range(0, len(triangle[i])):
+#             dp[i].append(-1)
+            
+#     # base-case
+#     dp[0][0] = triangle[0][0]
+
+    
+#     for i in range(1, N):
+#         for j in range(0, i + 1):
+#             if j == 0:
+#                 dp[i][j] = dp[i - 1][j] + triangle[i][j]
+                
+#             elif j == i:
+#                 dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
+                
+#             else:
+#                 dp[i][j] = max(dp[i - 1][j] + triangle[i][j], dp[i - 1][j - 1] + triangle[i][j])
+                
+#     answer = max(dp[N - 1])
+
+    
+#     return answer
+
+
 def solution(triangle):
     answer = 0
     N = len(triangle[len(triangle) - 1])
