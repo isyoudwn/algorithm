0bae6c90a50fcf33f30d8f27f739f0bf7984b93b
임채연 <dlacodus0407@gmail.com>
2024-09-02 17:15:52 +0900

    [leetcode 125] 유효한 팰린드롬 - 최적화

--- leet-code/week14/유효한 팰린드롬.py
+++ leet-code/week14/유효한 팰린드롬.py
@@ -1,36 +1,50 @@
+# class Solution:
+#     # convert all letters Upper to Lower
+#     # remove all non-alphanumeric characters -> remove all character which isn't number or alpha 
+#     # confirm the result phrase, having a same forward and backward(symmetry)
+#     # 200000 O(nlogn)
+
+#     def isPalindrome(self, s: str) -> bool:
+
+#         s.lower()
+#         s = s.lower().replace(' ', '')
+
+#         for st in s:
+#             if not st.isalnum():
+#                 s = s.replace(st, '')
+        
+#         N = len(s)
+
+#         if N <= 1:
+#             return True
+        
+
+#         forward = ''
+#         backward = ''
+
+#         if N % 2 == 0:
+#             forward = s[:N//2]
+#             backward = s[N//2:]
+#         else :
+#             forward = s[:N//2]
+#             backward = s[N//2 + 1:]
+
+#         backward = backward[::-1]
+
+#         if forward == backward:
+#             return True
+#         return False
+
+
 class Solution:
-    # convert all letters Upper to Lower
-    # remove all non-alphanumeric characters -> remove all character which isn't number or alpha 
-    # confirm the result phrase, having a same forward and backward(symmetry)
-    # 200000 O(nlogn)
-
     def isPalindrome(self, s: str) -> bool:
-
-        s.lower()
-        s = s.lower().replace(' ', '')
+        deq = deque()
 
         for st in s:
-            if not st.isalnum():
-                s = s.replace(st, '')
-        
-        N = len(s)
+            if st.isalnum():
+                deq.append(st.lower())
 
-        if N <= 1:
-            return True
-        
-
-        forward = ''
-        backward = ''
-
-        if N % 2 == 0:
-            forward = s[:N//2]
-            backward = s[N//2:]
-        else :
-            forward = s[:N//2]
-            backward = s[N//2 + 1:]
-
-        backward = backward[::-1]
-
-        if forward == backward:
-            return True
-        return False
+        while len(deq) > 1:
+            if deq.popleft() != deq.pop():
+                return False
+        return True
